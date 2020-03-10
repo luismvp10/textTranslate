23f0cffa00f715832a973f8fbc5c05b2e20e3450
luismvp10 <Luigid43>
2020-03-10 16:06:42 -0600

    app with translate module to make a multi language page

---
+++ src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app-translate';
+  constructor(public translate: TranslateService) {
+   /// translate.setDefaultLang('en');
+
+    translate.addLangs(['en', 'fr']);
+    if (localStorage.getItem('locale')) {
+      const browserLang = localStorage.getItem('locale');
+      translate.use(browserLang.match(/en|fr/) ? browserLang : 'en');
+    } else {
+      localStorage.setItem('locale', 'en');
+      translate.setDefaultLang('en');
+    }
+  }
+
+  changeLang(language: string) {
+    localStorage.setItem('locale', language);
+    this.translate.use(language);
+  }
+}
